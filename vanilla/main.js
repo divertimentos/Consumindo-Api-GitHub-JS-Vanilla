d4d57b03a6f5472efe9e27690e8c681a2b20a21c
guiemi <guiemi.teixeira@gmail.com>
2019-08-04 15:30:43 -0300

    Finaliza o app que consome informações de usuários do GitHub

--- vanilla/main.js
+++ vanilla/main.js
@@ -4,14 +4,22 @@
     const url = "https://api.github.com/users"
     const client_id = "9d71a8c4106f4ea74ef9"
     const client_secret = "f9ff9a14fad8c26a809624c6bd2007690311b4af"    
+    const count = 7
+    const sort = "created: ascs"
 
     async function getUser(user) {
         const profileResponse = await fetch(
             `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
             )
-        const profile = profileResponse.json()
+
+        const reposResponse = await fetch(
+            `${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`
+            )
+    
+        const profile = await profileResponse.json()
+        const repos = await reposResponse.json()
         
-        return profile
+        return {profile, repos}
     }
 
     function showProfile(user) {
@@ -29,15 +37,41 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-8"><div id="repos"></div></div>
+        <div class="col-md-8">
+            <div id="repos"></div>
+        </div>
     </div>`
+    }
+
+    function showRepos(repos) {
+        let output = ''
+
+        repos.forEach(repo => {
+            output += `
+            <div class="card card-body mt-2">
+            <div class="row">
+                <div class="col-md-6"><a href="${repo.html_url}" target="_black">${repo.name}</a></div>
+                <div class="col-md-6">
+                    <span class="badge badge-primary">stars: ${repo.stargazers_count}</span>
+                    <span class="badge badge-success">Watch: ${repo.watchers_count}</span>
+                    <span class="badge badge-warning">Forks: ${repo.forks_count}</span>
+                </div>
+            </div>
+        </div>`
+        })
+
+        document.getElementById("repos").innerHTML = output
     }
 
     search.addEventListener("keyup", e => {
         const user = e.target.value
 
         if (user.length > 0) {
-            getUser(user).then(res => showProfile(res))
+            getUser(user).then(res => {
+                showProfile(res.profile)
+                showRepos(res.repos)
+
+            })
         }
 
     })
